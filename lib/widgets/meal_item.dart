2f3dfd0efaaef2c549a49f8d5e8e7eca25dab566
adogbeji <baka_1993@yahoo.co.uk>
2024-06-22 10:50:07 +0100

    Add styling for meal title in MealItem widget

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -23,7 +23,33 @@ class MealItem extends StatelessWidget {
               bottom: 0,
               left: 0,
               right: 0,
-              child: Container(),
+              child: Container(
+                color: Colors.black54,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 6,
+                  horizontal: 44,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const Row()
+                  ],
+                ),
+              ),
             )
           ],
         ),
