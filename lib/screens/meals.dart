4813dd5b1252c0d67375576dd2b1b4a8d1b8f045
adogbeji <baka_1993@yahoo.co.uk>
2024-07-03 10:58:45 +0100

    Start using MealItem in meals screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/meal_item.dart';
+import 'package:meals_app/screens/meal_details_screen.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
 
   final String title;
   final List<Meal> meals;
+
+  void selectMeals(BuildContext context, Meal meal) {
+    // Navigator.of(context).pop();  // alt. Navigator.pop(context) => Removes screen manually
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(meal: meal),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +56,12 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (context, index) => MealItem(meal: meals[index])
+        itemBuilder: (context, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: (meal) {
+            selectMeals(context, meal);
+          },
+        ),
       );
     }
 
