3b91f0e699052aba4179d9027635a9212938fb51
adogbeji <baka_1993@yahoo.co.uk>
2024-07-05 11:01:33 +0100

    Output ingredients in meal details screen

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -22,7 +22,21 @@ class MealDetailsScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
 
-          SizedBox(height: 14,),
+          const SizedBox(height: 14,),
+
+          Text('Ingredients', style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),),
+
+          const SizedBox(height: 14,),
+
+          for (final ingredient in meal.ingredients)
+            Text(ingredient, style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          ),),
+
+          const SizedBox(height: 24,),
         ],
       ),
     );
