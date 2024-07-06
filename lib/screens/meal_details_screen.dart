d78154332c01bbc5947cbfa4a944b431261bd8d5
adogbeji <baka_1993@yahoo.co.uk>
2024-07-06 10:54:58 +0100

    Output step-by-step instructions for each meal

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -22,21 +22,60 @@ class MealDetailsScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
 
-          const SizedBox(height: 14,),
+          const SizedBox(
+            height: 14,
+          ),
 
-          Text('Ingredients', style: Theme.of(context).textTheme.titleLarge!.copyWith(
-            color: Theme.of(context).colorScheme.primary,
-            fontWeight: FontWeight.bold,
-          ),),
+          Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
 
-          const SizedBox(height: 14,),
+          const SizedBox(
+            height: 14,
+          ),
 
           for (final ingredient in meal.ingredients)
-            Text(ingredient, style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-            color: Theme.of(context).colorScheme.onBackground,
-          ),),
+            Text(
+              ingredient,
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
 
-          const SizedBox(height: 24,),
+          const SizedBox(
+            height: 24,
+          ),
+
+          Text(
+            'Steps',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+
+          const SizedBox(
+            height: 14,
+          ),
+
+          for (final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 8,
+              ),
+              child: Text(
+                step,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+            ),
         ],
       ),
     );
